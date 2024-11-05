9afd3023781d5eaa3e1d6606404bee8bf9bf3986
Chaakkii <tsangchakfung@gmail.com>
2024-11-05 12:06:22 +0200

    adText render and mkcert disabled

--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,18 @@
 import path from "path"
 import react from "@vitejs/plugin-react"
-import mkcert from'vite-plugin-mkcert'
+/*import mkcert from'vite-plugin-mkcert'*/
 import { defineConfig } from "vite"
 
 // https://vitejs.dev/config/
 export default defineConfig({
- plugins: [react(), mkcert()],
+ plugins: [react(), /*mkcert()*/],
  resolve: {
    alias: {
      "@": path.resolve(__dirname, "./src"),
    },
-   server: {
+   /*server: {
     https: true,
     host: true
-   }
+   }*/
  },
 })
